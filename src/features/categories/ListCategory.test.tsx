95d5e3764aae6e6a378f5a739ef125bad4cf9ebb
Vicente <vicente19981@live.com>
2023-07-11 15:08:01 -0300

    test: ensure ListCategory page change

--- src/features/categories/ListCategory.test.tsx
+++ src/features/categories/ListCategory.test.tsx
@@ -1,12 +1,20 @@
-import { renderWithProviders, screen } from "../../utils/test-utils";
+import {
+  fireEvent,
+  renderWithProviders,
+  screen,
+  waitFor,
+} from "../../utils/test-utils";
 import { baseUrl } from "../api/apiSlice";
 import { CategoryList } from "./ListCategory";
 import { rest } from "msw";
 import { setupServer } from "msw/node";
-import { categoryResponse } from "./mocks";
+import { categoryResponse, categoryResponsePage2 } from "./mocks";
 
 export const handlers = [
-  rest.get(`${baseUrl}/categories`, (_, res, ctx) => {
+  rest.get(`${baseUrl}/categories`, (req, res, ctx) => {
+    if (req.url.searchParams.get("page") === "2") {
+      return res(ctx.json(categoryResponsePage2), ctx.delay(150));
+    }
     return res(ctx.json(categoryResponse), ctx.delay(150));
   }),
 ];
@@ -28,5 +36,43 @@ describe("ListCategory", () => {
 
     const loading = screen.getByRole("progressbar");
     expect(loading).toBeInTheDocument();
+  });
+
+  it("should render list category correctly with data", async () => {
+    renderWithProviders(<CategoryList />);
+    await waitFor(() => {
+      const name = screen.getByText("Plum");
+      expect(name).toBeInTheDocument();
+    });
+  });
+
+  it("should render list category correctly with error", async () => {
+    server.use(
+      rest.get(`${baseUrl}/categories`, (_, res, ctx) => {
+        return res(ctx.status(500));
+      })
+    );
+    renderWithProviders(<CategoryList />);
+    await waitFor(() => {
+      const error = screen.getByText("Error fetching categories");
+      expect(error).toBeInTheDocument();
+    });
+  });
+
+  it("should handle on page change", async () => {
+    renderWithProviders(<CategoryList />);
+
+    await waitFor(() => {
+      const name = screen.getByText("Plum");
+      expect(name).toBeInTheDocument();
+    });
+
+    const nextButton = screen.getByTestId("KeyboardArrowRightIcon");
+    fireEvent.click(nextButton);
+
+    await waitFor(() => {
+      const name = screen.getByText("SeaGreen");
+      expect(name).toBeInTheDocument();
+    });
   });
 });
