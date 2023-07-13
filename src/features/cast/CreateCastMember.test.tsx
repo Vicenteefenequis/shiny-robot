78930d185d7ae2655e9726116dd7b504df13ff58
Vicente <vicente19981@live.com>
2023-07-13 14:20:54 -0300

    test: ensure CreateCastMember successfuly and error

--- src/features/cast/CreateCastMember.test.tsx
+++ src/features/cast/CreateCastMember.test.tsx
@@ -1,10 +1,68 @@
 import { CastMemberCreate } from "./CreateCastMember";
-import { renderWithProviders } from "../../utils/test-utils";
+import {
+  fireEvent,
+  renderWithProviders,
+  screen,
+  waitFor,
+} from "../../utils/test-utils";
+import { rest } from "msw";
+import { setupServer } from "msw/node";
+import { baseUrl } from "../api/apiSlice";
+
+const handlers = [
+  rest.post(`${baseUrl}/cast_members`, (_, res, ctx) => {
+    return res(ctx.delay(150), ctx.status(201));
+  }),
+];
+
+const server = setupServer(...handlers);
 
 describe("CreateCastMember", () => {
+  beforeAll(() => server.listen());
+  afterEach(() => server.resetHandlers());
+  afterAll(() => server.close());
+
   it("should render create cast member correctly", () => {
     const { asFragment } = renderWithProviders(<CastMemberCreate />);
 
     expect(asFragment()).toMatchSnapshot();
+  });
+
+  it("should handle create cast member success", async () => {
+    renderWithProviders(<CastMemberCreate />);
+
+    const name = screen.getByTestId("name");
+    const submit = screen.getByText("Save");
+
+    fireEvent.change(name, { target: { value: "Test" } });
+    fireEvent.click(submit);
+
+    await waitFor(() => {
+      expect(
+        screen.getByText("Success created cast member!")
+      ).toBeInTheDocument();
+    });
+  });
+
+  it("should handle submit error", async () => {
+    server.use(
+      rest.post(`${baseUrl}/cast_members`, (_, res, ctx) => {
+        return res(ctx.delay(150), ctx.status(500));
+      })
+    );
+
+    renderWithProviders(<CastMemberCreate />);
+
+    const name = screen.getByTestId("name");
+    const submit = screen.getByText("Save");
+
+    fireEvent.change(name, { target: { value: "Test" } });
+    fireEvent.click(submit);
+
+    await waitFor(() => {
+      expect(
+        screen.getByText("Error creating cast member!")
+      ).toBeInTheDocument();
+    });
   });
 });
